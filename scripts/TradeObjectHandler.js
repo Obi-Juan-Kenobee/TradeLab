c200016a332813c908ddc945079af6414e6885f8
Juan Martinez <jmart237@asu.edu>
2025-01-05 23:14:10 -0700

    - Fixed a few issues with the dashboard positioning - Moved Trade form function from TradeObjectHandler.js to tradeEntry.js - Fixed Long/Short button to indicate when it has been pressed.

--- scripts/TradeObjectHandler.js
+++ scripts/TradeObjectHandler.js
@@ -1,33 +1,3 @@
-// Trade class to handle trade data
-class Trade {
-  constructor(
-    symbol,
-    market,
-    entryPrice,
-    exitPrice,
-    quantity,
-    date,
-    notes,
-    direction
-  ) {
-    this.id = Date.now().toString();
-    this.symbol = symbol;
-    this.market = market;
-    this.entryPrice = entryPrice;
-    this.exitPrice = exitPrice;
-    this.quantity = quantity;
-    this.date = date;
-    this.notes = notes;
-    this.direction = direction; // 'long' or 'short'
-    this.profitLoss = this.calculateProfitLoss();
-  }
-
-  calculateProfitLoss() {
-    const rawPL = (this.exitPrice - this.entryPrice) * this.quantity;
-    return this.direction === "long" ? rawPL : -rawPL;
-  }
-}
-
 // Storage Strategy Interface
 class StorageStrategy {
   async loadTrades() {
